2edf68f144fd8c19320876b81b18d08b229d7f9f
Jittub45 <jitendrakumaryadav2003@gmail.com>
2025-08-17 23:55:18 +0530

    03: Clerk Authentication

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,17 +1,30 @@
-import { Button } from "@workspace/ui/components/button"
-import { add } from "@workspace/math/add"
-import { Input } from "@workspace/ui/components/input"
+"use client"
 
+import { SignInButton, UserButton } from "@clerk/nextjs";
+import { useMutation, useQuery, Authenticated, Unauthenticated } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
+import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
+  const users = useQuery(api.user.getMany);
+  const addUser = useMutation(api.user.ad);
+
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello apps/web</h1>
-        <Button size="sm">Button</Button>
-        <p>{add(2, 3)}</p>
-        <Input />
+    <>
+    <Authenticated>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <p>apps/web</p>
+        <UserButton />
+        <Button onClick={() => addUser()}>Add</Button>
+        <div className="max-w-sm w-full mx-auto">
+          {JSON.stringify(users, null, 2)}
+        </div>
       </div>
-    </div>
+    </Authenticated>
+    <Unauthenticated>
+      <p>Must be logged in!</p>
+      <SignInButton>Sign In</SignInButton>
+    </Unauthenticated>
+    </>
   )
 }
---
+++ packages/backend/convex/user.ts
@@ -0,0 +1,26 @@
+import { query, mutation } from "./_generated/server"
+
+export const getMany = query({
+    args: {},
+    handler: async (ctx) => {
+        const users = await ctx.db.query("users").collect();
+
+        return users;
+    }
+})
+
+export const ad = mutation({
+    args: {},
+    handler: async (ctx) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if (identity === null) {
+        throw new Error("Not authenticated");
+        }
+
+        const userId = await ctx.db.insert("users", {
+            name: "Jitendra",
+        })
+
+        return userId;
+    }
+});
